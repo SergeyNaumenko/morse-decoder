d3c1bbaf7fedd08f760bf1802e2f897385b360b1
Sergey <sergeynau@gmail.com>
2020-10-10 14:04:34 +0300

    feat: add morse-decoder solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    for (let i = 0; i < expr.length; i += 10){
+        const char = expr.substring(i, i+10);
+
+        if (char === '**********') {
+            result += ' ';
+            continue;
+        }
+        
+        result += getChar(char);
+    }
+
+    return result;
+}
+
+function getChar(str) {
+    let code = '';
+    for (let i = 0; i < str.length; i += 2) {
+        switch (`${str[i]}${str[i+1]}`) {
+            case '10':
+                code += '.'
+                break;
+            case '11':
+                code += '-'
+                break;
+            default: 
+                break;
+        }
+    }
+    return MORSE_TABLE[code];
 }
 
 module.exports = {
